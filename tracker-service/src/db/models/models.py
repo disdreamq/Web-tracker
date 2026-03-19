075d17c76bcbfb3a58671ecfae6f2f8258047f32
disdreamq <wlad050303@yandex.ru>
2026-03-19 15:32:28 +0300

    added site db model

---
+++ tracker-service/src/db/models/models.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.db.models.base import Base
+
+
+class Site(Base):
+    __tablename__ = "sites"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column(unique=True)
+    hash: Mapped[int]
+
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
