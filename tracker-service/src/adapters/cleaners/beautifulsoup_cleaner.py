2f5c6f1e67e292be4783da8ba546c738a6b19d80
disdreamq <wlad050303@yandex.ru>
2026-03-20 14:19:29 +0300

    added beautiful soup cleaner class

---
+++ tracker-service/src/adapters/cleaners/beautifulsoup_cleaner.py
@@ -0,0 +1,40 @@
+from bs4 import BeautifulSoup
+
+from src.exceptions import UnexpectedException
+from src.interfaces.abstract_cleaner_repository import ICleanerRepository
+
+
+class Cleaner(ICleanerRepository):
+    def clear_html(self, html_page: str) -> str:
+        try:
+            soup = BeautifulSoup(html_page, "lxml")
+
+            for tag in soup(
+                [
+                    "script",
+                    "style",
+                    "noscript",
+                    "meta",
+                    "link",
+                    "head",
+                    "nav",
+                    "footer",
+                    "header",
+                    "form",
+                    "button",
+                    "input",
+                    "aside",
+                    "svg",
+                    "iframe",
+                    "object",
+                    "embed",
+                ]
+            ):
+                tag.decompose()
+
+            for tag in soup.find_all(True):
+                tag.attrs = {}
+
+            return soup.get_text(strip=True)
+        except Exception as e:
+            raise UnexpectedException(f"Unexpected error: {e}") from e
