1d007acdf70c79a41fe648d2262d271f0e2eb0c7
disdreamq <wlad050303@yandex.ru>
2026-03-20 14:20:11 +0300

    refactored httpx client, updated with exceptions

--- tracker-service/src/adapters/http_clients/httpx_client.py
+++ tracker-service/src/adapters/http_clients/httpx_client.py
@@ -1,10 +1,25 @@
 import logging
 
-from httpx import AsyncClient, Response
+from httpx import (
+    AsyncClient,
+    ConnectError,
+    InvalidURL,
+    NetworkError,
+    Response,
+    TimeoutException,
+)
 
+from src.exceptions import (
+    PageConnectionError,
+    PageFetchError,
+    PageInvalidURLError,
+    PageTimeoutError,
+    UnexpectedException,
+)
 from src.interfaces.abstract_http_client_repository import IHTTPClientRepository
 
 logger = logging.getLogger(__name__)
+
 
 class HTTPXClient(IHTTPClientRepository):
     async def get(self, url: str) -> Response:
@@ -12,8 +27,15 @@ class HTTPXClient(IHTTPClientRepository):
             async with AsyncClient() as client:
                 response = await client.get(url)
             if response.status_code != 200:
-                raise #TODO добавить доменные исключения
+                raise PageFetchError(
+                    "Page fetch error", status_code=response.status_code
+                )
             return response
-        except Exception: #TODO доделать тут ошибки
-            raise
-
+        except TimeoutException as e:
+            raise PageTimeoutError(f"Timeout fetching {url}") from e
+        except (ConnectError, NetworkError) as e:
+            raise PageConnectionError(f"Network error fetching {url}") from e
+        except InvalidURL as e:
+            raise PageInvalidURLError(f"Invalid URL: {url}") from e
+        except Exception as e:
+            raise UnexpectedException(f"Unexpected error: {e}") from e
