2343379d3f0339103fa53b8f518556fc442c0cae
disdreamq <wlad050303@yandex.ru>
2026-03-20 17:32:58 +0300

    refactoring

---
+++ tracker-service/src/interfaces/abstract_db_repository.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+class IDBepository[T](ABC):
+    """Interface for db repositories with base CRUD.
+
+    Raises:
+        NotImplementedError for any not implemented methods.
+    """
+
+    @abstractmethod
+    async def create(self, *args, **kwargs) -> T | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_id(self, *args, **kwargs) -> T | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_by_url(self, *args, **kwargs) -> T | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def update(self, *args, **kwargs) -> T | None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def delete(self, *args, **kwargs) -> bool:
+        raise NotImplementedError
