c064ce8dab8f30e483de0eed3e40d1e14a83ff88
disdreamq <wlad050303@yandex.ru>
2026-03-26 14:05:19 +0300

    refactored service with pydantic schemas instead of dataclasses

--- tracker-service/src/site/service.py
+++ tracker-service/src/site/service.py
@@ -5,7 +5,7 @@ import validators
 from src.core.deco_for_SQLAlchemy_exc import handle_service_exceptions
 from src.core.exceptions import UniqueURLError
 from src.interfaces.abstract_db_repository import IDBepository
-from src.site.DTO import SiteDTO
+from src.site.schemas import SSiteCreate, SSiteDTO
 
 logger = getLogger(__name__)
 
@@ -16,58 +16,34 @@ class SiteService:
         self.repo = repo
 
     @handle_service_exceptions
-    async def create(self, url: str, hash: str) -> SiteDTO:
-        if not validators.url(url):
+    async def create(self, site: SSiteCreate) -> SSiteDTO:
+        if not validators.url(SSiteCreate.url):
             raise ValueError
 
-        if self.repo.get_by_url(url):
+        if self.repo.get_by_url(site.url):
             raise UniqueURLError(
-                f"Cannot add site to database, site already exists: {url=}"
+                f"Cannot add site to database, site already exists: {site.url=}"
             )
 
-        site_in_db = await self.repo.create(url=url, hash=hash)
-        return SiteDTO(
-            id=site_in_db.id,
-            url=site_in_db.url,
-            hash=site_in_db.hash,
-            created_at=site_in_db.created_at,
-            updated_at=site_in_db.updated_at,
-        )
+        site_in_db = await self.repo.create(url=site.url, hash=site.hash)
+        return SSiteDTO.model_validate(site_in_db)
 
     @handle_service_exceptions
-    async def get_by_url(self, url: str) -> SiteDTO | None:
+    async def get_by_url(self, url: str) -> SSiteDTO | None:
         if site := await self.repo.get_by_url(url):
-            return SiteDTO(
-                id=site.id,
-                url=site.url,
-                hash=site.hash,
-                created_at=site.created_at,
-                updated_at=site.updated_at,
-            )
+            return SSiteDTO.model_validate(site)
         return
 
     @handle_service_exceptions
-    async def get_by_id(self, id: int) -> SiteDTO | None:
+    async def get_by_id(self, id: int) -> SSiteDTO | None:
         if site := await self.repo.get_by_id(id):
-            return SiteDTO(
-                id=site.id,
-                url=site.url,
-                hash=site.hash,
-                created_at=site.created_at,
-                updated_at=site.updated_at,
-            )
+            return SSiteDTO.model_validate(site)
         return
 
     @handle_service_exceptions
-    async def update(self, url, hash_to_update) -> SiteDTO | None:
+    async def update(self, url, hash_to_update) -> SSiteDTO | None:
         if site := await self.repo.update(url, hash_to_update):
-            return SiteDTO(
-                id=site.id,
-                url=site.url,
-                hash=site.hash,
-                created_at=site.created_at,
-                updated_at=site.updated_at,
-            )
+            return SSiteDTO.model_validate(site)
         return
 
     @handle_service_exceptions
