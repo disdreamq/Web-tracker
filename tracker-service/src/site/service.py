6eeebcdcb89fd91d76c89f8dff63083d667dc2a5
disdreamq <wlad050303@yandex.ru>
2026-03-27 15:58:38 +0300

    added stream method for service to avoid memory overflow

--- tracker-service/src/site/service.py
+++ tracker-service/src/site/service.py
@@ -1,3 +1,4 @@
+from collections.abc import AsyncGenerator
 from logging import getLogger
 
 from src.core.deco_for_SQLAlchemy_exc import handle_service_exceptions
@@ -15,14 +16,22 @@ class SiteService(ISiteService):
         self.repo = repo
 
     @handle_service_exceptions
-    async def create(self, site: SSiteCreate) -> SSiteDTO:
+    async def create(self, site_to_create: SSiteCreate) -> SSiteDTO:
 
-        if self.repo.get_by_url(site.url):
+        if self.repo.get_by_url(site_to_create.url):
+            logger.exception(
+                f"Unique url exception during create site, url already exists: url= {site_to_create.url}"
+            )
             raise UniqueURLError(
-                f"Cannot add site to database, site already exists: {site.url=}"
+                f"Cannot add site to database, site already exists: {site_to_create.url=}"
             )
 
-        site_in_db = await self.repo.create(url=site.url, hash=site.hash)
+        site_in_db = await self.repo.create(
+            url=site_to_create.url, hash=site_to_create.hash
+        )
+        logger.info(
+            f"Added site to database: url = {site_to_create.url}, hash = {site_to_create.hash}"
+        )
         return SSiteDTO.model_validate(site_in_db)
 
     @handle_service_exceptions
@@ -40,9 +49,20 @@ class SiteService(ISiteService):
     @handle_service_exceptions
     async def update(self, url, hash_to_update) -> SSiteDTO | None:
         if site := await self.repo.update(url, hash_to_update):
+            logger.info(
+                f"Updated site to database: url = {site.url}, hash = {site.hash}"
+            )
             return SSiteDTO.model_validate(site)
         return
 
     @handle_service_exceptions
+    async def get_sites_stream(self) -> AsyncGenerator:
+        stream = await self.repo.get_sites_stream()
+        async for site in stream:
+            url = site.url, hash = site.hash
+            yield (url, hash)
+
+    @handle_service_exceptions
     async def delete(self, url) -> bool:
+        logger.info(f"Deleted site from database: {url=}")
         return await self.repo.delete(url)
