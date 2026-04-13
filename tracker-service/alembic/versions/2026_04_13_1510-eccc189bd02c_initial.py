76ba6d043ae7583dfd250b969be01f90928f5aba
disdreamq <wlad050303@yandex.ru>
2026-04-13 16:23:09 +0300

    added initial migration

--- tracker-service/alembic/versions/2026_04_13_1510-eccc189bd02c_initial.py
+++ tracker-service/alembic/versions/2026_04_13_1510-eccc189bd02c_initial.py
@@ -1,8 +1,8 @@
-"""initial migration
+"""initial
 
-Revision ID: 8a94f111845d
+Revision ID: eccc189bd02c
 Revises: 
-Create Date: 2026-03-26 15:16:40.261642
+Create Date: 2026-04-13 15:10:09.773144
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '8a94f111845d'
+revision: str = 'eccc189bd02c'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
