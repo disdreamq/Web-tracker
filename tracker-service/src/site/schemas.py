0ed20e643df948e5b50c8542967c94b9cf464d80
disdreamq <wlad050303@yandex.ru>
2026-04-23 17:14:22 +0300

    added doc strings

--- tracker-service/src/site/schemas.py
+++ tracker-service/src/site/schemas.py
@@ -2,11 +2,28 @@ from pydantic import BaseModel, ConfigDict, HttpUrl
 
 
 class SSiteCreate(BaseModel):
+    """
+    Schema for creating a new site.
+
+    Attributes:
+        url: Site URL (validated HttpUrl).
+        hash: Content hash of the site.
+    """
+
     url: HttpUrl
     hash: str
 
 
 class SSiteDTO(BaseModel):
+    """
+    Data Transfer Object for site entity.
+
+    Attributes:
+        id: Unique identifier of the site.
+        url: Site URL.
+        hash: Current content hash of the site.
+    """
+
     model_config = ConfigDict(from_attributes=True)
 
     id: int
