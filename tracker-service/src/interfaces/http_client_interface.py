f4c2d855c7db02e66e9b68b45edb7d524ede7e24
disdreamq <wlad050303@yandex.ru>
2026-04-23 17:14:51 +0300

    added doc strings

--- tracker-service/src/interfaces/http_client_interface.py
+++ tracker-service/src/interfaces/http_client_interface.py
@@ -4,7 +4,17 @@ from typing import TypeVar
 T = TypeVar("T")
 
 class IHTTPClientRepository[T](ABC):
-    """Interface for HTTP client"""
+    """Interface for HTTP client."""
+
     @abstractmethod
     async def get(self, url: str) -> T:
+        """
+        Fetch content from URL.
+
+        Args:
+            url: URL to fetch.
+
+        Returns:
+            HTTP response.
+        """
         pass
