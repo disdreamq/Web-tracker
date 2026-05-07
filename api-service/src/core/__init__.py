55dae13b210c05887834f6c5a4ebe2a25d4da0ee
disdreamq <wlad050303@yandex.ru>
2026-05-07 16:30:51 +0300

    added inits

---
+++ api-service/src/core/__init__.py
@@ -0,0 +1,4 @@
+from .config import Settings, get_settings
+from .exceptions import *  # noqa: F403
+
+__all__ = ["Settings", "get_settings"]
---
+++ api-service/src/db/infrastructure/__init__.py
@@ -0,0 +1,3 @@
+from .session import AsyncSessionLocal, engine
+
+__all__ = ["engine", "AsyncSessionLocal"]
