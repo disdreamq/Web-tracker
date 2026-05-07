1a8a30d1b649e2c333da41d7bf97b7aaca0dbade
disdreamq <wlad050303@yandex.ru>
2026-05-07 16:20:19 +0300

    added init in user

---
+++ api-service/src/user/__init__.py
@@ -0,0 +1,13 @@
+from src.user.model import User
+from src.user.repository import SQLAlchemyUserRepository
+from src.user.schemas import UserCreate, UserDTO, UserUpdate
+from src.user.service import UserService
+
+__all__ = [
+    "SQLAlchemyUserRepository",
+    "User",
+    "UserCreate",
+    "UserDTO",
+    "UserService",
+    "UserUpdate",
+]
