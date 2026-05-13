49c56c68c3c3672b7b2cecb92d314371c2ba8e54
disdreamq <vlaseyar@gmail.com>
2026-05-13 13:11:37 +0300

    refactoring

--- api-service/src/websocket/dependencies.py
+++ api-service/src/websocket/dependencies.py
@@ -3,5 +3,5 @@ from src.websocket.connection_manager import ConnectionManager
 manager = ConnectionManager()
 
 
-def get_manager() -> ConnectionManager:
+async def get_manager() -> ConnectionManager:
     return manager
