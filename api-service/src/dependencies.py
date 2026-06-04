016e912971e0b7c75d8fc5ade304567ec99e4ff3
disdreamq <vlaseyar@gmail.com>
2026-06-04 15:47:00 +0300

    added dependency for producer and user service providing

--- api-service/src/dependencies.py
+++ api-service/src/dependencies.py
@@ -1,6 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+from src.db.dependencies import get_session_factory
 from src.interfaces.rabbit_producer_interface import IRabbitMQProducer
 from src.rabbitmq.rabbit_publisher import RabbitMQProducer
+from src.user.repository import SQLAlchemyUserRepository
+from src.user.service import UserService
 
 
 async def get_producer() -> IRabbitMQProducer:
     return RabbitMQProducer()
+
+
+async def get_user_service(
+    session_factory: Annotated[
+        async_sessionmaker[AsyncSession], Depends(get_session_factory)
+    ],
+) -> UserService:
+    return UserService(SQLAlchemyUserRepository(session_factory))
